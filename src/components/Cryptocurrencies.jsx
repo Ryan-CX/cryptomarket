34fff108413ec13605935fc0966c53b6f56e0378
Ryan (Chenguang) Xu <xcg1234@gmail.com>
2021-10-11 20:54:13 -0400

    Update Cryptocurrencies.jsx

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
+import millify from 'millify';
+import { Link } from 'react-router-dom';
+import { Card, Row, Col, Input } from 'antd';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const Cryptocurrencies = () => {
+	const { data, cryptoList, isFetching } = useGetCryptosQuery();
+	const [cryptos, setcryptos] = useState(cryptoList?.data?.coins);
+	console.log(cryptos);
+
 	return <div>Crpt</div>;
 };
 
